106fe11238aad35ff0e3a0b547cbdd8e6279c0ab
Altamsh Bairagdar <84860267+altamsh04@users.noreply.github.com>
2025-06-21 00:38:23 +0530

    Update layout margin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,14 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { JetBrains_Mono } from "next/font/google"
+import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 
-const jetbrainsMono = JetBrains_Mono({
-  subsets: ["latin"],
-  variable: "--font-jetbrains-mono",
-})
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Altamsh Bairagdar - Portfolio",
-  description: "Full Stack Developer",
-    generator: 'v0.dev'
+  description: "A Full Stack Developer Who Loves Backend Most",
 }
 
 export default function RootLayout({
@@ -22,8 +18,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${jetbrainsMono.variable} font-mono`}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange={false}>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
